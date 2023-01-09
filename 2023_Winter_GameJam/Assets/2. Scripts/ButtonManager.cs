44b6dbf80ba6f48596fe1fc0a6fb54665acafc68
minseo <cms20061112@gmail.com>
2023-01-09 15:30:23 +0900

    UI구성
    대략적인 Title UI구성

---
+++ 2023_Winter_GameJam/Assets/2. Scripts/ButtonManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void OnClickStaerBtn()
+    {
+        
+    }
+
+    public void OnClickSetButton()
+    {
+
+    }
+
+    public void OnClickEscBtn()
+    {
+
+    }
+}
---
+++ 2023_Winter_GameJam/Assets/2. Scripts/PlayerMove.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Animator animator;
+    Rigidbody2D rigidbody2D;
+
+    [SerializeField]
+    private float speed = 7;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Move();
+        MoveAnimation();
+    }
+
+    void Move()
+    {
+        rigidbody2D.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * speed;
+    }
+
+    private void MoveAnimation()
+    {
+        animator.SetFloat("InputX", rigidbody2D.velocity.x);
+        animator.SetFloat("InputY", rigidbody2D.velocity.y);
+    }
+}
