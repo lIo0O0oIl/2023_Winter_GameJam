770929c107d42c88a4baa917eb8edc93f3614f50
minseo <cms20061112@gmail.com>
2023-01-11 01:15:37 +0900

    모험

---
+++ 2023_Winter_GameJam/Assets/2. Scripts/Minseo/EarthwormMove.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthwormMove : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 2;
+
+
+}
