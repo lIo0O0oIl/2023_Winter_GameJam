33471c586b6cf64c567b3214367de65555253016
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2023-01-11 11:46:34 +0900

    퍼즐 2까지 진짜 다 완성
    아니면 자살하면 그만이야~

--- 2023_Winter_GameJam/Assets/2. Scripts/SungEun/Puzzle_2/PlaneRotatePuzzle.cs
+++ 2023_Winter_GameJam/Assets/2. Scripts/SungEun/Puzzle_2/PlaneRotatePuzzle.cs
@@ -17,47 +17,40 @@ public class PlaneRotatePuzzle : MonoBehaviour
     {
         float z = plane.transform.eulerAngles.z - 90;
 
-        plane.transform.DORotate(new Vector3(0, 0, z), 2f).SetEase(Ease.Linear);
+        plane.transform.DORotate(new Vector3(0, 0, z), 2f).SetEase(Ease.InCubic);
 
-        StartCoroutine(ColliderFalse(true));
+        StartCoroutine(ColliderFalse());
     }
 
     public void LeftButton()
     {
         float z = plane.transform.eulerAngles.z + 90;
 
-        plane.transform.DORotate(new Vector3(0, 0, z), 2f).SetEase(Ease.Linear);
+        plane.transform.DORotate(new Vector3(0, 0, z), 2f).SetEase(Ease.InCubic);
 
-        StartCoroutine(ColliderFalse(false));
+        StartCoroutine(ColliderFalse());
     }
 
-    IEnumerator ColliderFalse(bool right)
+    IEnumerator ColliderFalse()
     {
-        if (right)
-        {
-            rightButton.GetComponent<CircleCollider2D>().enabled = false;
-            goal.GetComponent<Rigidbody2D>().gravityScale = 0;
-            yield return new WaitForSeconds(2f);
-            goal.GetComponent<Rigidbody2D>().gravityScale = 1;
-            yield return new WaitForSeconds(.1f);
-            rightButton.GetComponent<CircleCollider2D>().enabled = true;
-        }
-        else
-        {
-            leftButton.GetComponent<CircleCollider2D>().enabled = false;
-            goal.GetComponent<Rigidbody2D>().gravityScale = 0;
-            yield return new WaitForSeconds(2f);
-            goal.GetComponent<Rigidbody2D>().gravityScale = 1;
-            yield return new WaitForSeconds(.1f);
-            leftButton.GetComponent<CircleCollider2D>().enabled = true;
-        }
+        rightButton.GetComponent<CircleCollider2D>().enabled = false;
+        leftButton.GetComponent<CircleCollider2D>().enabled = false;
+        resetButton.GetComponent<CircleCollider2D>().enabled = false;
+        //goal.GetComponent<Rigidbody2D>().gravityScale = 0;
+        yield return new WaitForSeconds(2.5f);
+        //goal.GetComponent<Rigidbody2D>().gravityScale = 1;
+        //yield return new WaitForSeconds(.1f);
+        rightButton.GetComponent<CircleCollider2D>().enabled = true;
+        leftButton.GetComponent<CircleCollider2D>().enabled = true;
+        resetButton.GetComponent<CircleCollider2D>().enabled = true;
+        //yield return null;
     }
 
     public void ReSetButton()
     {
         //plane.transform.Rotate(0, 0, 180);
         //goal.transform.position = new Vector3(1.197f, 3.054f, 0f);
-        plane.transform.Rotate(planeVector);
+        plane.transform.rotation = Quaternion.Euler(planeVector);
         goal.transform.position = goalVector;
     }
 }
