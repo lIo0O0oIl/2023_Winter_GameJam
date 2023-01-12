b7c27e307e9bedb6ec01ed2f408a3ec50d128b73
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2023-01-12 20:51:33 +0900

    으악

--- 2023_Winter_GameJam/Assets/2. Scripts/SungEun/Question1.cs
+++ 2023_Winter_GameJam/Assets/2. Scripts/SungEun/Question1.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class Question1 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static Question1 Instance { get; private set; }
+
+    public GameObject[] question;
+
+    public int level = 1;
+
+    private void Start()
     {
-        
+        Instance = this;
+        level = 1;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Qusetion(bool a)
     {
-        
+        if (a)
+        {
+            if (level == 1)
+            {
+                question[0].SetActive(true);
+            }
+            else if (level == 2)
+            {
+                question[1].SetActive(true);
+            }
+            else
+            {
+                question[2].SetActive(true);
+            }
+        }
+        else
+        {
+            if (level == 1)
+            {
+                question[0].SetActive(false);
+            }
+            else if (level == 2)
+            {
+                question[1].SetActive(false);
+            }
+            else
+            {
+                question[2].SetActive(false);
+            }
+        }
     }
 }
